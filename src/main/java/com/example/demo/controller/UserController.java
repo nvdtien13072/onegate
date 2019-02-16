6c3ddd990ef48f200b9b1e5a7b1974b62802a7d5
tlien <nvdtien13072>
2019-02-16 23:55:46 +0700

    Hoan thanh api user

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,15 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "welcome";
+    }
+
 }
